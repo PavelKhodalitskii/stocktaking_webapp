e72132f574ca5f20b1846a8c7b48e9279df8dff7
Pavel <dreamsobenatic00@mail.ru>
2024-04-13 18:19:29 +0800

    Basic admin views was added

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import CustomUser
 # Register your models here.
+
+@admin.register(CustomUser)
+class CustomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'slug')
