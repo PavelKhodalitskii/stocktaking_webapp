45f3cee9a39e78158f9b46b389473176dc4ee54a
Pavel <dreamsobenatic00@mail.ru>
2024-04-19 19:12:35 +0800

    Items related models was initialized

--- items_management/models.py
+++ items_management/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 
-from account.models import CustomUser
+from account.models import CustomUser, Office
 
 # Create your models here.
 
@@ -15,11 +16,29 @@ class InventoryItems(models.Model):
     name = models.CharField(max_length=255, verbose_name="Имя")
     value = models.FloatField(default=0.00, verbose_name="Стоимость")
     amount = models.IntegerField(null=False)
-    # status = models.ForeignKey()
+    assessed_value = models.FloatField(default=0.00, verbose_name="Оценочная стоимость")
+    status = models.ForeignKey('Status', related_name="inventory_items", null=True, blank=True, on_delete=models.SET_NULL)
+    office = models.ForeignKey(Office, related_name="inventory_items", null=True, blank=True, on_delete=models.SET_NULL)
+    type = models.ForeignKey('ItemType', related_name="inventory_items", null=True, blank=True, on_delete=models.SET_NULL)
     financially_responsible_person = models.ForeignKey(CustomUser, related_name="related_items", blank=True, null=True, on_delete=models.CASCADE)
+    valid_from = models.DateTimeField(verbose_name="Действительно с", default=timezone.now)
 
     def __str__(self):
         return str(self.name) + str(self.item_number)
     
     def get_absolute_url(self):
         return reverse('inventory_item', kwargs={'item_slug': self.slug})
+    
+class ItemType(models.Model):
+    class Meta:
+        verbose_name = 'Тип предметов инвентаря'
+        verbose_name_plural = 'Типы предметов инвентаря'
+
+    name = models.CharField(max_length=75, verbose_name='Имя типа')
+
+class Status(models.Model):
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+    name = models.CharField(max_length=75, verbose_name='Имя статуса')
