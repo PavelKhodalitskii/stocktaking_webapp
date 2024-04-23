55705eeb60cfbc9f89caeada8f20e6254ced0a09
Pavel <dreamsobenatic00@mail.ru>
2024-04-23 18:38:54 +0800

    Djoser package for auth was added

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include, re_path
 from django.contrib.auth.views import LogoutView
 
 # from .views import 
@@ -7,6 +7,8 @@ from .views import user_login, logout_user, LoginUser
 urlpatterns = [
     # path('login/', user_login, name='login'),
     path('login/', LoginUser.as_view(), name='login'),
-    path('logout/', logout_user, name='logout')
+    path('logout/', logout_user, name='logout'),
+    path('api/v1/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken'))  
 ]
 
