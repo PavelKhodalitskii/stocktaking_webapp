5067905c2c68bc2a12693dbe3e8e9a40c121a104
Evolvento <zelent.nikita@mail.ru>
2024-04-27 13:18:00 +0300

    str added

--- licences_management/models.py
+++ licences_management/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.urls import reverse
+
+from account.models import CustomUser
 
 # Create your models here.
 
@@ -7,11 +10,20 @@ class Licence(models.Model):
     class Meta:
         verbose_name = 'Лицензия'
         verbose_name_plural = 'Лицензии'
+
+    slug = models.SlugField(max_length=255, unique=True, verbose_name="URL лицензии")
     name = models.CharField(max_length=75, verbose_name="Имя лицензии")
     #data = models.DateTimeField()
     valid_from = models.DateTimeField(verbose_name="Действительно с")
     valid_to = models.DateTimeField(verbose_name="Действительно по")
-    type = models.ForeignKey('LicenceType', related_name='licences', null=True, on_delete=models.SET_NULL)
+    type = models.ForeignKey('LicenceType', related_name='licence', null=True, on_delete=models.SET_NULL)
+    users = models.ManyToManyField(CustomUser, related_name='licences')
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('licence', kwargs={'licence_slug': self.slug})
 
 
 class LicenceType(models.Model):
@@ -19,5 +31,8 @@ class LicenceType(models.Model):
         verbose_name = 'Тип'
         verbose_name_plural = 'Типы'
     name = models.CharField(max_length=75, verbose_name="Имя типа")
+
+    def __str__(self):
+        return self.name
 
 
--- reports/models.py
+++ reports/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 from items_management.models import InventoryItems, ItemType, Status
@@ -9,9 +10,20 @@ class StocktalkingReport(models.Model):
     class Meta:
         verbose_name = 'Отчёт'
         verbose_name_plural = 'Отчёт'
-    author = models.ForeignKey(InventoryItems, related_name='reports', null=True, on_delete=models.SET_NULL)
-    invent = models.ForeignKey(ItemType, related_name='reports', null=True, on_delete=models.SET_NULL)
-    type = models.ForeignKey('Invents', related_name='reports', null=True, on_delete=models.SET_NULL)
+        
+
+    author = models.ForeignKey(InventoryItems, related_name='report', null=True, on_delete=models.SET_NULL)
+    invent = models.ForeignKey(ItemType, related_name='report', null=True, on_delete=models.SET_NULL)
+    type = models.ForeignKey('Invents', related_name='report', null=True, on_delete=models.SET_NULL)
+    items = models.ManyToManyField(InventoryItems, through='RelationItemsReports', related_name='reports')
+    slug = models.SlugField(max_length=255, unique=True, verbose_name="URL лицензии")
+    note = models.CharField(max_length=255, verbose_name="Примечание")
+
+    def __str__(self):
+        return "Отчёт инвентаризации " + str(self.pk)
+    
+    def get_absolute_url(self):
+        return reverse('stocktalking_report', kwargs={'report_slug': self.slug})
 
 
 class Invents(models.Model):
@@ -22,6 +34,9 @@ class Invents(models.Model):
     data_end = models.DateTimeField()
     responsible_person = models.ForeignKey(CustomUser, related_name='invents', null=True, on_delete=models.SET_NULL)
     office_building = models.ForeignKey(OfficeBuilding, related_name='invents', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return "Инвентаризация " + str(self.pk)
 
 
 class RelationItemsReports(models.Model):
