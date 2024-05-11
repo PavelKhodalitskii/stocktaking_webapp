7db88b90dd4c26af811423f52a03e031518a3fe6
pleiada <dreamsobenatic00@mail.ru>
2024-05-11 16:29:39 +0800

    Refactoring

--- items_management/serizalizers.py
+++ items_management/serizalizers.py
@@ -1,45 +1,8 @@
 from rest_framework import serializers
 
-from account.models import CustomUser, Role, OfficeBuilding, Office
+from account.models import Office
+from account.serializers import OfficeSerializer
 from .models import InventoryItems
-from reports.models import StocktalkingReport, RelationItemsReports
-
-
-
-#Account serializers
-class RoleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Role
-        fields = "__all__"
-
-class OfficeBuildingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OfficeBuilding
-        fields = "__all__"
-
-class OfficeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Office
-        fields = "__all__"
-
-class CustomUserSerializer(serializers.ModelSerializer):
-    role = serializers.SerializerMethodField('get_role')
-    office_building = serializers.SerializerMethodField('get_office_building')
-
-    class Meta:
-        model = CustomUser
-        fields = ("id", "username", "first_name", "last_name", "is_superuser", "role", "office_building", "slug")
-
-    def get_role(self, obj):
-        if obj.role:
-            role = Role.objects.get(id=obj.role.id)
-            return RoleSerializer(role).data
-    
-    def get_office_building(self, obj):
-        if obj.office_building:
-            office_building = OfficeBuilding.objects.get(id=obj.office_building.id)
-            return OfficeBuildingSerializer(office_building).data
-
 
 #Items managemetn serializers
 class InventoryItemsSerializer(serializers.ModelSerializer):
@@ -53,27 +16,3 @@ class InventoryItemsSerializer(serializers.ModelSerializer):
         if obj.office:
             office = Office.objects.get(id=obj.office.id)
             return OfficeSerializer(office).data
-        
-
-#Reports serializers
-class StocktalkingListSerizalizer(serializers.ModelSerializer):
-    author = serializers.SerializerMethodField('get_author')
-    items = serializers.SerializerMethodField('get_items')
-
-    class Meta:
-        model = StocktalkingReport
-        fields = ('id', 'author', 'ivent', 'type', 'items', 'slug', 'note')
-
-
-    def get_author(self, obj):
-        author = CustomUser.objects.get(id=obj.author.id)
-        return CustomUserSerializer(author).data
-    
-    def get_items(self, obj):
-        items = obj.items.all()
-        return InventoryItemsSerializer(items, many=True).data
-
-class RelationItemsReportsSerizalizer(serializers.ModelSerializer):
-    class Meta:
-        model = RelationItemsReports
-        fields = "__all__"
--- items_management/views.py
+++ items_management/views.py
@@ -1,20 +1,14 @@
 from typing import Any
 
 from rest_framework import generics
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
 
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import InventoryItems, ItemType
 from account.models import CustomUser, Office
-from reports.models import StocktalkingReport, RelationItemsReports
 
-from .serizalizers import InventoryItemsSerializer, StocktalkingListSerizalizer, RelationItemsReportsSerizalizer
+from .serizalizers import InventoryItemsSerializer
 from .permissions import IsOwner
 
 
@@ -94,67 +88,3 @@ class ItemDetailView(DetailView):
 class InvenoryItemsAPIView(generics.ListAPIView):
     queryset = InventoryItems.objects.all()
     serializer_class = InventoryItemsSerializer
-
-class StocktalkingListAPIView(APIView):
-    permission_classes = (IsAdminUser, IsOwner)
-
-    def get(self, request):
-        report = StocktalkingReport.objects.get(author__id = request.user.id)
-        items = report.items.all()
-        rel_info = RelationItemsReports.objects.all().filter(report=report)
-
-        return Response({'report': StocktalkingListSerizalizer(report, many=False).data,
-                         'relations_info': RelationItemsReportsSerizalizer(rel_info, many=True).data
-                         })
-
-class ReportItemsApiView(APIView):
-    permission_classes = (IsAdminUser, IsOwner)
-    serializer_class = RelationItemsReportsSerizalizer
-    post_fields_required = ['datatime', 'item', 'report']
-    put_fields_forbidden = ['id', 'item', 'report']
-
-    def validate_post_data(self, data):
-        self.post_fields_required = ['datatime', 'item', 'report']
-
-        for field in self.post_fields_required:
-            if not field in data.keys():
-                return False
-
-        return True
-    
-    def validate_put_data(self, data):
-        self.put_fields_forbidden = ['id', 'item', 'report']
-
-        for field in self.put_fields_forbidden:
-            if not field in data.keys():
-                return False
-
-        return True
-    
-    def get(self, request, item_id):
-        object = RelationItemsReports.objects.get(id=item_id)
-        if object:
-            return Response({'item': RelationItemsReportsSerizalizer(object, many=False).data
-                            })
-        return Response({"status": "No object with such id"})
-    
-    def post(self, request, item_id):
-        serializer = RelationItemsReportsSerizalizer(data=request.data)
-        if serializer.is_valid() and self.validate_post_data(request.data):
-            serializer.save()
-            return Response({"status": "Item successfuly added to relation"})
-        else:
-            return Response({"status": "Wrong data provided"})
-        
-    def put(self, request, item_id):
-        object = RelationItemsReports.objects.get(id = item_id)
-        if self.validate_put_data(request.data):
-            if object:
-                serializer = RelationItemsReportsSerizalizer(object, data=request.data)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response({"status": "Item successfuly updated"})
-                else:
-                    return Response({"status": "Wrong data provided"})
-            return Response({"status": "No object with such id"})
-        return Response({"status": f"Forbidden fields change. Note: it's forbidden to change fields: {self.put_fields_forbidden}"})
