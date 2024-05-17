0075cc619089689c20232bf8ac807fc96cf1e08f
pleiada <dreamsobenatic00@mail.ru>
2024-05-17 22:24:12 +0800

    CustomUser serizalizer fix

--- account/serializers.py
+++ account/serializers.py
@@ -18,17 +18,11 @@ class OfficeSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    role = serializers.SerializerMethodField('get_role')
     office_building = serializers.SerializerMethodField('get_office_building')
 
     class Meta:
         model = CustomUser
-        fields = ("id", "username", "first_name", "last_name", "is_superuser", "role", "office_building", "slug")
-
-    def get_role(self, obj):
-        if obj.role:
-            role = Role.objects.get(id=obj.role.id)
-            return RoleSerializer(role).data
+        fields = ("id", "username", "first_name", "last_name", "is_superuser", "office_building", "slug")
     
     def get_office_building(self, obj):
         if obj.office_building:
